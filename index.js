dcf09986f7e4f0be90a6cf1ae7995dc48c9d6d74
Matthew Kenny <matthewkenny@gmail.com>
2017-07-03 15:49:25 -0700

    Need to re-try "failed" responses from the server

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// Literally straight from the docs.
-// https://www.npmjs.com/package/request-promise
-// http://api.nytimes.com/svc/archive/v1/2016/11.json?api-key={your-api-key}
-// https://www.npmjs.com/package/bottleneck
+//TODO: Need to implement a re-try if the response is not JSON Object, but string
 
 console.time('execution');
 const rp = require('request-promise');
 const Bottleneck = require('bottleneck');
 const util = require('util');
 const jsonfile = require('jsonfile');
+const Promise = require("bluebird");
+const fs = require('fs');
 
+Promise.longStackTraces();
 // This stuff could be wrapped in a cmd-line param or class config
 const base_url = 'http://api.nytimes.com/svc/archive/v1'
 const api_key = '772925f7d490445fa8a6b1be09ec262a'
-const months = [1,2];
+const months = [1, 2];
 const year = 1985;
 
-function makeRequestOptionsObj (base_url, year, month, api_key) {
+function makeRequestOptionsObj(base_url, year, month, api_key) {
     let uri_constructed = util.format('%s/%s/%s.json', base_url, year, month);
     return {
         uri: uri_constructed,
@@ -24,13 +24,12 @@ function makeRequestOptionsObj (base_url, year, month, api_key) {
         },
         headers: {
             'User-Agent': 'Request-Promise'
-        },
-        json: true // Automatically parses the JSON string in the response 
+        }
     }
 }
 
 let request_options = [];
-months.forEach(function(month) {
+months.forEach(function (month) {
     request_options.push(makeRequestOptionsObj(base_url, year, month, api_key))
 })
 
@@ -38,32 +37,36 @@ months.forEach(function(month) {
 var limiter = new Bottleneck(10, 3000, null, null, true);
 
 function makeThrottledRequest(option_obj) {
-    return limiter.schedule(rp, option_obj);
+    console.log('in makeThrottledRequest');
+    console.log(option_obj);
+    function rp2 (option_obj) {
+        console.log('in rp2');
+        return rp(option_obj).catch(err => {console.log('rp2 rp error')});
+    };
+    return limiter.schedule(rp2, option_obj).catch(err => {console.log('schedule error')});
 }
 
 function makeRequests(option_array) {
     // Given an array of promises, return Promise.all()
     console.log('Executing makeRequests');
     return Promise.all(option_array.map(makeThrottledRequest));
-}; 
+};
 
-var data = makeRequests(request_options);
+makeRequests(request_options).then(responses => {
+    // Write out `docs` array for each month to a single json file for the year.
+    let path = util.format('./data-by-year/%s.json', year);
 
-// Called after Promise.all() is fufilled
-data.then(responses => { 
-  // Write out `docs` array for each month to a single json file for the year.
-  let path = util.format('./data-by-year/%s.json', year);
+    let merged_docs = [];
+    // responses.forEach(response => {
+    //     let response_json = JSON.parse(response);
+    //     response_json['response']['docs'].forEach(doc => {
+    //         merged_docs.push(doc);
+    //     });
+    // });
 
-  let merged_docs = [];
-  responses.forEach(response => {
-      console.log(response);
-      response['response']['docs'].forEach(doc => {
-          merged_docs.push(doc);
-      });
-    });
-
-  jsonfile.writeFile(path, {"docs": merged_docs}, function (err) {console.error(err)});
-  console.timeEnd('execution');
-}, reason => {
-  console.log(reason)
+    // jsonfile.writeFile(path, { "docs": merged_docs }, function (err) { console.error(err) });
+    fs.writeFile(path, responses);
+    console.timeEnd('execution');
+}).catch(err => {
+    console.log(err);
 });
