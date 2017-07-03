fcfda3b2864ba7b7069863c0e527c482dd192646
Matthew Kenny <matthewkenny@gmail.com>
2017-07-03 13:58:40 -0700

    let's figure out promise stuff

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 // Literally straight from the docs.
 // https://www.npmjs.com/package/request-promise
 // http://api.nytimes.com/svc/archive/v1/2016/11.json?api-key={your-api-key}
+// https://www.npmjs.com/package/bottleneck
 
-// LET'S TRY THIS: https://www.npmjs.com/package/bottleneck
+console.time('execution');
 const rp = require('request-promise');
-const throttle = require('promise-ratelimit')(5000);
+const Bottleneck = require('bottleneck');
+const util = require('util');
+const jsonfile = require('jsonfile');
 
-var options = {
-    uri: 'http://api.nytimes.com/svc/archive/v1/1984/8.json',
-    qs: {
-        'api-key': '772925f7d490445fa8a6b1be09ec262a' // -> uri + '?access_token=xxxxx%20xxxxx' 
-    },
-    headers: {
-        'User-Agent': 'Request-Promise'
-    },
-    json: true // Automatically parses the JSON string in the response 
-};
+// This stuff could be wrapped in a cmd-line param or class config
+const base_url = 'http://api.nytimes.com/svc/archive/v1'
+const api_key = '772925f7d490445fa8a6b1be09ec262a'
+const months = [1,2];
+const year = 1985;
 
-var options2 = {
-    uri: 'http://api.nytimes.com/svc/archive/v1/1909/8.json',
-    qs: {
-        'api-key': '772925f7d490445fa8a6b1be09ec262a' // -> uri + '?access_token=xxxxx%20xxxxx' 
-    },
-    headers: {
-        'User-Agent': 'Request-Promise'
-    },
-    json: true // Automatically parses the JSON string in the response 
-};
-
-function makeThrottledRequest(option_obj) {
-    // I think what is happening here is that
-    // both requests are going off at nearly the same time,
-    // just after a 5 second delay.
-    return throttle().then(rp(option_obj));
+function makeRequestOptionsObj (base_url, year, month, api_key) {
+    let uri_constructed = util.format('%s/%s/%s.json', base_url, year, month);
+    return {
+        uri: uri_constructed,
+        qs: {
+            'api-key': api_key // -> uri + '?access_token=xxxxx%20xxxxx' 
+        },
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // Automatically parses the JSON string in the response 
+    }
 }
 
+let request_options = [];
+months.forEach(function(month) {
+    request_options.push(makeRequestOptionsObj(base_url, year, month, api_key))
+})
+
+// 10 concurrent requests, 3000ms between requests, rejectOnDrop is true
+var limiter = new Bottleneck(10, 3000, null, null, true);
+
+function makeThrottledRequest(option_obj) {
+    return limiter.schedule(rp, option_obj);
+}
 
 function makeRequests(option_array) {
     // Given an array of promises, return Promise.all()
@@ -42,26 +47,23 @@ function makeRequests(option_array) {
     return Promise.all(option_array.map(makeThrottledRequest));
 }; 
 
-var data = makeRequests([options, options2]);
-console.log(data);
+var data = makeRequests(request_options);
 
-data.then(values => { 
-  console.log(values);
+// Called after Promise.all() is fufilled
+data.then(responses => { 
+  // Write out `docs` array for each month to a single json file for the year.
+  let path = util.format('./data-by-year/%s.json', year);
+
+  let merged_docs = [];
+  responses.forEach(response => {
+      console.log(response);
+      response['response']['docs'].forEach(doc => {
+          merged_docs.push(doc);
+      });
+    });
+
+  jsonfile.writeFile(path, {"docs": merged_docs}, function (err) {console.error(err)});
+  console.timeEnd('execution');
 }, reason => {
   console.log(reason)
 });
-
-// rp(options2)
-//     .then(function (response) {
-//         console.log('1901 Response Fin');
-//     })
-//     .catch(function (err) {
-//         // API call failed... 
-//     });
-// rp(options)
-//     .then(function (response) {
-//         console.log('1986 Response Fin');
-//     })
-//     .catch(function (err) {
-//         // API call failed... 
-//     });
