fc23416b2347a10ac2a3dca2fc134ee44041e4b3
Matthew Kenny <matthewkenny@gmail.com>
2017-07-06 22:21:57 -0700

    before trying to implement a promise

--- query_nyt_archive_api.js
+++ query_nyt_archive_api.js
@@ -1,5 +1,3 @@
-//TODO: Need to implement a re-try if the response is not JSON Object, but string
-
 const rp = require('request-promise');
 const Bottleneck = require('bottleneck');
 const util = require('util');
@@ -9,7 +7,7 @@ const jsonfile = require('jsonfile');
 const base_url = 'http://api.nytimes.com/svc/archive/v1'
 // const base_url = 'http://localhost:53821/svc/archive/v1'
 const api_key = '772925f7d490445fa8a6b1be09ec262a'
-const months = [1, 2, 3,4,5,6,7,8,9,10,11,12];
+const months = [1, 2, 3,];
 const year = 1985;
 
 let months_processed = 0;
@@ -18,63 +16,59 @@ function makeRequestOptionsObj(base_url, year, month, api_key) {
     let uri_constructed = util.format('%s/%s/%s.json', base_url, year, month);
     return {
         uri: uri_constructed,
-        qs: {
-            'api-key': api_key // -> uri + '?access_token=xxxxx%20xxxxx' 
-        },
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
+        qs: { 'api-key': api_key },
+        headers: { 'User-Agent': 'Request-Promise' },
         json: true
     }
 }
 
-let request_options = [];
-months.forEach(function (month) {
-    request_options.push(makeRequestOptionsObj(base_url, year, month, api_key))
-})
-
 let merged_docs = [];
 
 function makeRequest(options) {
-// Function that:
-// - Makes a request
-// - then checks if the response is valid json
-// - retry if response is not valid json 
+    // Function that:
+    // - Makes a request
+    // - then checks if the response is valid json
+    // - retry if response is not valid json 
     return rp(options)
-    .then((response) => {
-        // Change to === 'number' to force an error
-        if (typeof(response) === 'object') {
-            console.log(options.uri + 'Reponse contained valid JSON');
+        .then((response) => {
+            // Change to === 'number' to force an error
+            if (typeof (response) !== 'object') {
+                console.log(options.uri + ' request failed, throwing error');
+                throw TypeError;
+            }
+            console.log(options.uri + ' Response contained valid JSON');
             // increment months_processed
             months_processed++;
             // add docs to merged_docs array
             response['response']['docs'].forEach(doc => {
                 merged_docs.push(doc);
             });
-            // this can probably go into a separate function
+            return response;
+        })
+        // This will catch an error `thrown` in the `then` above.
+        .catch((error) => {
+            console.log('in the rp.catch() error');
+            makeThrottledRequest(options);
+        });
+}
+
+function makeThrottledRequest(options) {
+    console.log(options.uri + ' in makeThrottledRequest');
+    return limiter.schedule(makeRequest, options)
+        .then((value) => {
+            console.log('returned from in makeThrottledRequest');
+
             if (months_processed === months.length) {
                 let path = util.format('./data-by-year/%s.json', year);
                 jsonfile.writeFile(path, { "docs": merged_docs }, function (err) { console.error(err) });
             }
-            return response;
-        } else {
-            console.log(options.uri + ' request failed, retrying');
-            limiter.schedule(makeRequest, options);
-        }
-    })
-    // This will catch an error `thrown` in the `then` above.
-    .catch((error) => {
-        console.log('in the rp.catch() error');
-        throw error;
-    });
+        })
+        .catch(err => { console.log(err) });
 }
 
 // 10 concurrent requests, 3000ms between requests, rejectOnDrop is true
 var limiter = new Bottleneck(10, 3000, null, null, true);
 
-function makeThrottledRequest(options) {
-    console.log(options.uri + ' in makeThrottledRequest');
-    return limiter.schedule(makeRequest, options).catch(err => {console.log(err)});
-}
-
+// Create a new array with request option objs for each month.
+let request_options = months.map(month => makeRequestOptionsObj(base_url, year, month, api_key));
 request_options.map(makeThrottledRequest);
