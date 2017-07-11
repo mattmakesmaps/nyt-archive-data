f30394344f6579763539fe9f1efdf412fbd804b4
Matt Kenny <mkenny@mkenny-linux.tsi.lan>
2017-07-10 17:43:23 -0700

    reformat to module

--- query_nyt_archive_api.js
+++ query_nyt_archive_api.js
@@ -1,15 +1,11 @@
 const Bottleneck = require('bottleneck');
-const util = require('util');
-const jsonfile = require('jsonfile');
-const request = require('request');
 const promiseRetry = require('promise-retry');
+const request = require('request');
+const util = require('util');
 
-// This stuff could be wrapped in a cmd-line param or class config
 const base_url = 'http://api.nytimes.com/svc/archive/v1'
 // const base_url = 'http://localhost:53821/svc/archive/v1'
-const api_key = '772925f7d490445fa8a6b1be09ec262a'
 const months = [1,2];
-const year = 1985;
 
 function makeRequestOptionsObj(base_url, year, month, api_key) {
     let uri_constructed = util.format('%s/%s/%s.json', base_url, year, month);
@@ -26,7 +22,7 @@ function makeRequest(options) {
     return new Promise((resolve, reject) => {
         request.get(options, (error, response, body) => {
             if (error || typeof (body) !== 'object') {
-                console.log(options.uri + ' request failed, throwing error');
+                // console.log(options.uri + ' request failed, throwing error');
                 reject(error);
             }
             resolve(body);
@@ -42,33 +38,27 @@ function makeRetryRequest(options) {
 }
 
 function makeThrottledRequest(options) {
-    console.log(options.uri + ' in makeThrottledRequest');
     return limiter.schedule(makeRetryRequest, options)
 }
 
 // 10 concurrent requests, 3000ms between requests, rejectOnDrop is true
-var limiter = new Bottleneck(10, 3000, null, null, true);
+var limiter = new Bottleneck(10, 5000, null, null, true);
 
-function queryYearOfData(year, api_key) {
+module.exports = (year, api_key) => {
     // Create a new array with request option objs for each month.
     let request_options = months.map(month => makeRequestOptionsObj(base_url, year, month, api_key));
 
     return Promise.all(request_options.map(makeThrottledRequest))
     .then((resolved_responses) => {
         return new Promise((resolve, reject) => {
-            console.log('Promise.all() Has resolved.');
-            console.log(resolved_responses.length);
             let merged_docs = [];
             resolved_responses.forEach(response => {
                 response['response']['docs'].forEach(doc => {
                     merged_docs.push(doc);
                 });
             });
-            resolve(merged_docs);
+            resolve({"docs": merged_docs});
         });
     })
     .catch((error) => console.log(error));
 }
-
-docs1985 = queryYearOfData(1985, api_key);
-docs1985.then(docs => console.log(docs))
--- test_request.js
+++ test_request.js
@@ -1,56 +1,14 @@
+const query_nyt = require('./query_nyt_archive_api.js');
+const jsonfile = require('jsonfile');
 const util = require('util');
-const request = require('request');
-const promiseRetry = require('promise-retry');
-
-const base_url = 'http://localhost:53821/svc/archive/v1'
-const api_key = '772925f7d490445fa8a6b1be09ec262a'
-const months = [1, 2];
+const api_key = '772925f7d490445fa8a6b1be09ec262a';
 const year = 1985;
 
-function makeRequestOptionsObj(base_url, year, month, api_key) {
-    let uri_constructed = util.format('%s/%s/%s.json', base_url, year, month);
-    return {
-        uri: uri_constructed,
-        qs: {
-            'api-key': api_key // -> uri + '?access_token=xxxxx%20xxxxx' 
-        },
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true
-    }
-}
+let q = query_nyt(year, api_key);
+console.log('q: ' + q);
 
-function makeRequest(options) {
-    // Returns request wrapped in Promise
-    return new Promise((resolve, reject) => {
-        request.get(options, (error, response, body) => {
-            if (error || typeof (body) !== 'object') {
-                console.log(options.uri + ' request failed, throwing error');
-                reject(error);
-            }
-            resolve(body);
-        })
-    });
-}
-
-function makeRetryRequest(options) {
-    // Returns makeRequest Promise wrapped in Retry Promise
-    return promiseRetry(function(retry) {
-        return makeRequest(options).catch(retry);
-    });
-}
-
-let request_params = makeRequestOptionsObj(base_url, 1985, 1, api_key);
-
-let response = makeRetryRequest(request_params)
-    .then((value) => {
-        console.log('Chained response');
-    })
-    .catch((error) => {
-        console.log('in the makeRequest.catch() error');
-        console.log(error);
-    })
-
-// A pending promise
-console.log(response);
+q.then((data) => {
+    console.log('Done!');
+    let path = util.format('./data-by-year/%s.json', year);
+    jsonfile.writeFile(path, data, function (err) { console.error(err) });
+}).catch((error) => {console.log(error)});
